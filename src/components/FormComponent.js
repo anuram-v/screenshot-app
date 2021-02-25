2012a840ef9968508712fbbd8da2fa3735563e38
anuram-v <79614424+anuram-v@users.noreply.github.com>
2021-02-25 16:08:07 +0530

    Style modifed

--- src/components/FormComponent.js
+++ src/components/FormComponent.js
@@ -72,7 +72,8 @@ export default function FormComponent() {
                     font-size: 16px;
                     color: black;
                     font-style: italic;
-                    margin-left:30px
+                    margin-left:30px:
+                    margin-top: 0px;
                 }
             `}</style>
         </div>
